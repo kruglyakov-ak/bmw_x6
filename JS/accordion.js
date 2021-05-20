5cd03a54f56761b66770f2ed42a69d8b75012736
Aleksandr <aleksandr.k.kruglyackov@yandex.ru>
2021-05-20 23:14:36 +0300

    Create accordion.js and connect to index.html

---
+++ JS/accordion.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const featureLinkElems = document.querySelectorAll('.feature__link');
+    const featureSubElems = document.querySelectorAll('.feature-sub');
+
+    featureLinkElems.forEach((btn, index) => {
+        btn.addEventListener('click', () => {
+            if (btn.classList.contains('feature__link_active')) {
+                featureSubElems[index].classList.add('hidden');
+                btn.classList.remove('feature__link_active');
+            } else {
+            featureLinkElems.forEach((featureLinkElem) => {
+                featureLinkElem.classList.remove('feature__link_active');
+            })
+            featureSubElems.forEach((featureSubElem) => {
+                featureSubElem.classList.add('hidden');
+            })
+
+            featureSubElems[index].classList.toggle('hidden');
+            btn.classList.toggle('feature__link_active');
+            }
+        })
+    })
+});
